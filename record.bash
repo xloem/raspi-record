0ee94f7d15c5f469529ec0b582dc985391d6e379
xloem <0xloem@gmail.com>
2021-02-18 08:00:49 -0500

    stores tar files with git-annex

--- record.bash
+++ record.bash
@@ -1,20 +1,33 @@
 #!/usr/bin/env bash
 
-macs="MAC--$(ip addr show | sed -ne 's/.*link\/ether \([0-9a-fA-F:]*\).*/\1/p' | tr '\n' -)"
+macs="MAC--$(ip addr show | sed -ne 's/.*link\/ether \([0-9a-fA-F:]*\).*/\1/p' | tr '\n:' -_)"
 uuids="$(lsblk -o UUID | tr '\n' -)"
 name="$(hostname)-${uuids%-}-${macs%-}"
-# {
-# 	if ! [ -e .git/annex ]
-# 	then
-# 		name="$(hostname)"-"$IP"
-# 		echo $name
-# 		git annex init "$name"
-# 	fi
-# 	git annex add *.mkv
-# 	git annex sync
-# 	git annex copy --all --to=skynet
-# } &
-#GST_DEBUG=3 gst-launch-1.0 v4l2src do-timestamp=true ! queue ! videoconvert ! omxh264enc ! h264parse config-interval=-1 ! matroskamux streamable=true ! filesink location=$(date --iso=seconds)-"$(hostname)".mkv
+GIT="$(type -p git)"
+git()
+{
+	# removes error cruft from git-annex's git somehow being unable to open a preloaded library
+	"$GIT" "$@" 2>&1 | grep -v libarmmem 1>&2
+}
+if ! [ -e .git ]
+then
+	git init .
+	git commit --allow-empty -m "$name"
+fi
+if ! [ -e .git/annex ]
+then
+	echo $name
+	git annex init "$name"
+	git config annex.thin true
+	git annex adjust --unlock
+	git annex initremote skynet chunk=64MiB type=external encryption=none externaltype=siaskynet
+fi
+git annex add *.tar
+git commit -m "$(date +%Y)-${name}"
+{
+	git annex sync
+	git annex copy --all --to=skynet --jobs=2
+} &
 GST_DEBUG=3 gst-launch-1.0 v4l2src ! videoconvert ! avenc_h264_omx ! matroskamux streamable=true ! fdsink | streamtar "$(date +%Y)-${name}.tar" "$(date --iso=seconds).mkv"
-#plan is to make a small supporting c program to append to tar files from live streams, and store them in annex repositories
-
+git annex add *.tar
+git commit -m "$(date +%Y)-${name}"
