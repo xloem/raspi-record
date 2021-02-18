06be15fef33317e47ebb62f81c8abb4838bd2019
xloem <0xloem@gmail.com>
2021-02-18 06:29:03 -0500

    streamtar really helps this work well

--- record.bash
+++ record.bash
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+macs="MAC--$(ip addr show | sed -ne 's/.*link\/ether \([0-9a-fA-F:]*\).*/\1/p' | tr '\n' -)"
+uuids="$(lsblk -o UUID | tr '\n' -)"
+name="$(hostname)-${uuids%-}-${macs%-}"
 # {
 # 	if ! [ -e .git/annex ]
 # 	then
-# 		IP="$(ip addr show | grep inet\  | sed -ne 's/.*inet \([0-9\.]*\)\/.*global.*/\1/p')"
 # 		name="$(hostname)"-"$IP"
 # 		echo $name
 # 		git annex init "$name"
@@ -12,7 +14,7 @@
 # 	git annex sync
 # 	git annex copy --all --to=skynet
 # } &
-#GST_DEBUG=3 gst-launch-1.0 v4l2src do-timestamp=true ! queue ! videoconvert ! omxh264enc ! h264parse config-interval=-1 ! matroskamux ! filesink location=$(date --iso=seconds)-"$(hostname)".mkv
-GST_DEBUG=3 gst-launch-1.0 v4l2src ! videoconvert ! avenc_h264_omx ! matroskamux ! filesink location=$(date --iso=seconds)-"$(hostname)".mkv
+#GST_DEBUG=3 gst-launch-1.0 v4l2src do-timestamp=true ! queue ! videoconvert ! omxh264enc ! h264parse config-interval=-1 ! matroskamux streamable=true ! filesink location=$(date --iso=seconds)-"$(hostname)".mkv
+GST_DEBUG=3 gst-launch-1.0 v4l2src ! videoconvert ! avenc_h264_omx ! matroskamux streamable=true ! fdsink | streamtar "$(date +%Y)-${name}.tar" "$(date --iso=seconds).mkv"
 #plan is to make a small supporting c program to append to tar files from live streams, and store them in annex repositories
 
