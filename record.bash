1bb764d871439883e9a738ed17b6bd2b95a7a49a
xloem <0xloem@gmail.com>
2021-02-18 02:08:49 -0500

    -

--- record.bash
+++ record.bash
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
-{
-	git annex add *.mkv
-	git annex sync
-	git annex copy --all --to=skynet
-} &
-gst-launch-1.0 v4l2src ! omxh264enc ! mkvmux ! filesink=$(date --iso=seconds)-"$hostname".mkv
+# {
+# 	if ! [ -e .git/annex ]
+# 	then
+# 		IP="$(ip addr show | grep inet\  | sed -ne 's/.*inet \([0-9\.]*\)\/.*global.*/\1/p')"
+# 		name="$(hostname)"-"$IP"
+# 		echo $name
+# 		git annex init "$name"
+# 	fi
+# 	git annex add *.mkv
+# 	git annex sync
+# 	git annex copy --all --to=skynet
+# } &
+#GST_DEBUG=3 gst-launch-1.0 v4l2src do-timestamp=true ! queue ! videoconvert ! omxh264enc ! h264parse config-interval=-1 ! matroskamux ! filesink location=$(date --iso=seconds)-"$(hostname)".mkv
+GST_DEBUG=3 gst-launch-1.0 v4l2src ! videoconvert ! avenc_h264_omx ! matroskamux ! filesink location=$(date --iso=seconds)-"$(hostname)".mkv
+#plan is to make a small supporting c program to append to tar files from live streams, and store them in annex repositories
+
