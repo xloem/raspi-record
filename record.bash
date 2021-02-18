8e7b0433820af55b7ba6c7d70063cc54c1435756
xloem <0xloem@gmail.com>
2021-02-18 10:48:07 -0500

    git-annex is kinda working now

--- record.bash
+++ record.bash
@@ -4,10 +4,15 @@ macs="MAC--$(ip addr show | sed -ne 's/.*link\/ether \([0-9a-fA-F:]*\).*/\1/p' |
 uuids="$(lsblk -o UUID | tr '\n' -)"
 name="$(hostname)-${uuids%-}-${macs%-}"
 GIT="$(type -p git)"
+absorb_raspiweirdness()
+{
+	grep -v libarmmem 1>&2
+}
 git()
 {
 	# removes error cruft from git-annex's git somehow being unable to open a preloaded library
-	"$GIT" "$@" 2>&1 | grep -v libarmmem 1>&2
+	set -o pipefail
+	"$GIT" "$@" 2>&1 | absorb_raspiweirdness
 }
 if ! [ -e .git ]
 then
@@ -18,16 +23,24 @@ if ! [ -e .git/annex ]
 then
 	echo $name
 	git annex init "$name"
-	git config annex.thin true
 	git annex adjust --unlock
 	git annex initremote skynet chunk=64MiB type=external encryption=none externaltype=siaskynet
 fi
 git annex add *.tar
 git commit -m "$(date +%Y)-${name}"
-{
-	git annex sync
+tarfile="$(date +%Y)-${name}.tar"
+while ps $$ >/dev/null 2>&1
+do
 	git annex copy --all --to=skynet --jobs=2
-} &
-GST_DEBUG=3 gst-launch-1.0 v4l2src ! videoconvert ! avenc_h264_omx ! matroskamux streamable=true ! fdsink | streamtar "$(date +%Y)-${name}.tar" "$(date --iso=seconds).mkv"
+	git annex sync
+	while git annex dropunused 1; do sleep 1; done
+	sleep 10
+	(
+		flock 9 || exit 1
+		git annex add "$tarfile"
+		git commit -m "$(date +%Y)-${name}"
+	) 9<"$tarfile"
+done 2>&1 | absorb_raspiweirdness &
+GST_DEBUG=3 gst-launch-1.0 v4l2src ! videoconvert ! avenc_h264_omx ! matroskamux streamable=true ! fdsink | streamtar "$tarfile" "$(date --iso=seconds).mkv"
 git annex add *.tar
 git commit -m "$(date +%Y)-${name}"
